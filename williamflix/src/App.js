e46251c906319394745e803686860340536695c5
William Rodrigues de Sousa <rm78369@fiap.com.br>
2020-07-28 08:03:34 -0300

    :star: now the project seems like a real website, but I need to make anything different :star:

--- williamflix/src/App.js
+++ williamflix/src/App.js
@@ -1,12 +1,51 @@
 import React from 'react';
+import Menu from './components/Menu'
+import dadosIniciais from './data/dados_iniciais.json';
+import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-      Alguma coisa ai no meio
-      JSX -> [J]ava[S]cript [X]ML
-      Update automático (famoso hot reload)
+    <div style={{ background: "#141414" }}>
+      <Menu />
+
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[1]}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[2]}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[3]}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[4]}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[5]}
+      />
+
+      <Footer />
     </div>
   );
 }

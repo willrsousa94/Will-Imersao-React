d01fe81cd1b8399f8396bcc53c51170f1b8ffbfc
William Rodrigues de Sousa <rm78369@fiap.com.br>
2020-07-27 22:48:57 -0300

    adding the react project, erased some files that will not be used

---
+++ williamflix/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      Alguma coisa ai no meio
+      JSX -> [J]ava[S]cript [X]ML
+      Update automático (famoso hot reload)
+    </div>
+  );
+}
+
+export default App;
